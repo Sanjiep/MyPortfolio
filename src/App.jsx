fa6ba242364a62f7069c004d289247afcc8e3af3
sanjiep.magar <saasmagar554@gmail.com>
2024-09-12 23:10:59 +0300

    Fixed deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Marquee from './components/Marquee'
 import Projects from './components/Projects'
