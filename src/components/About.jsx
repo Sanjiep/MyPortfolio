ab1b6affb19b72e150b61a44e644abe0625ac23a
sanjiep.magar <saasmagar554@gmail.com>
2024-09-16 21:52:50 +0300

    changes in about features

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,10 @@ import { ABOUT } from "../constants"
 const About = () => {
   return (
     <section data-scroll-section data-scroll data-scroll-speed='-0.2'  data-scroll-sticky
-    data-scroll-target="#about-target" // The target for sticky behavior
+    data-scroll-target="#about-target"
     id="about" className="bg-black py-8 rounded-t-3xl max-h-full">
 
-        <h2 id="about" className="text-center text-3xl lg:text-8xl">About Me</h2>
+        <h2 className="text-center text-3xl lg:text-8xl">About Me</h2>
         <div className="flex items-center justify-center">
             <p className="m-8 max-w-6xl text-3xl lg:text-6xl">{ABOUT}</p>
         </div>
